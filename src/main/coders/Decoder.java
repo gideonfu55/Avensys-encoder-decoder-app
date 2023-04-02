bbbe1dd18d140bcecc73db660108aa940176bcc2
jcmonkey87 <fuzhonglong@gmail.com>
2023-04-02 21:23:13 +0800

    update decode() to change first char to account for lowercases in input

--- src/main/coders/Decoder.java
+++ src/main/coders/Decoder.java
@@ -18,7 +18,7 @@ public class Decoder extends CoderMain{
    * 
    */
   public String decode(String encodedText) {
-    char offset = encodedText.charAt(0);
+    char offset = Character.toUpperCase(encodedText.charAt(0));
     if (encodedText == null || encodedText.isEmpty()) return "There is no input.";
     if (REFERENCE_TABLE_LIST.indexOf(offset) == -1) return "The first character is not a valid offset.";
 
